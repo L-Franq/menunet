971535b4c14e943b5dd833ccae851675db0f4155
L-Franq <lothfranqueira1@gmail.com>
2026-08-05 19:08:30 +0100

    frontend - add default index for select input

--- databases/db.js
+++ databases/db.js
@@ -3,7 +3,12 @@ const { Pool }  = require("pg");
 
 const db = new Pool({
   connectionString: process.env.DB_URL,
-  ssl: process.env.DB_URL ? {rejectUnauthorized: false} : false
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: process.env.NODE_ENV === "production" ? {rejectUnauthorized: false} : false
 });
 
 /*Local PostgreSQL Config
